26be6d34cfa23987aa713047cec0c3f164f10961
Redd2020 <dab09267@gmailcom>
2021-08-20 16:57:37 -0500

    a

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -2317,7 +2317,7 @@ class BattleStatGuesser {
 		} else if (abilityid === 'unburden' || abilityid === 'speedboost' || abilityid === 'motordrive') {
 			isFast = true;
 			moveCount['Ultrafast'] = 1;
-		} else if (abilityid === 'chlorophyll' || abilityid === 'swiftswim' || abilityid === 'sandrush' || abilityid === 'desertdash') {
+		} else if (abilityid === 'chlorophyll' || abilityid === 'swiftswim' || abilityid === 'sandrush') {
 			isFast = true;
 			moveCount['Ultrafast'] = 2;
 		} else if (itemid === 'salacberry') {
