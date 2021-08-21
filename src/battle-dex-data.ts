e8b64869803fb60fd55c20b621f63f37cc4713bf
Redd2020 <dab09267@gmailcom>
2021-08-20 20:46:49 -0500

    a

--- src/battle-dex-data.ts
+++ src/battle-dex-data.ts
@@ -1081,6 +1081,7 @@ interface MoveFlags {
 	pulse?: 1 | 0;
 	/** Power is multiplied by 1.2 when used by a Pokemon with the Iron Fist Ability. */
 	punch?: 1 | 0;
+	kick?: 1 | 0;
 	/** If this move is successful, the user must recharge on the following turn and cannot make a move. */
 	recharge?: 1 | 0;
 	/** Bounced back to the original user by Magic Coat or the Magic Bounce Ability. */
