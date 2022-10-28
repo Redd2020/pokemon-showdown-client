17eab9f7cac30ad6c3a021c03729393e24aa7844
Redd2020 <dab09267@gmail.com>
2022-10-28 11:45:43 -0500

    c

--- src/battle-tooltips.ts
+++ src/battle-tooltips.ts
@@ -1413,7 +1413,7 @@ class BattleTooltips {
 		const allowTypeOverride = !noTypeOverride.includes(move.id);
 		
 		if (allowTypeOverride && move.id === 'explosion' && !move.isZ) {
-            if (value.abilityModify(0, 'Supernova') && moveType !== 'Fire') moveType = 'Fire';
+            if (value.abilityModify(0, 'Supernova')) moveType = 'Fire';
         }
 
 		if (allowTypeOverride && category !== 'Status' && !move.isZ && !move.id.startsWith('hiddenpower')) {
